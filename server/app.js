08ff21c4864c34ed0f70dcb6ace8ad97e5ff6137
Sidharth Ponram <sidharthponram@upunikself.xyz>
2025-12-14 23:06:34 -0500

    deploy on render ready

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -33,6 +34,12 @@ async function startDatabase() {
 }
 
 startDatabase();
+
+app.use(express.static(path.join(__dirname, "../client/build"))); 
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Starting WorldBucketList Server @ ${PORT}`);
